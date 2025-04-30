54a3bb3cb0c97af6f2f9f23b2d9ea3d9a011ff47
saitec <dlinares@rrhh.luz.edu.ve>
2025-04-30 11:19:54 -0400

    1.- Terminada función select   1.1 - Reiniciar valores de los campos al seleccionar nuevo elemento   1.2 - Indicar el monto del concepto al seleccionarse el elemento   1.3 - Indicar el monto a cobrar si es posible al ingresar el valor del monto y el saldo.   1.4 - Agregar el indice del concepto.
    2 - Funcion calculo.
      2.1 - Obtener valor del campo saldo
      2.2 - Obtener el valor del monto previo o valor del concepto.
      2.3 - Crear el sub total del valor previo y el saldo(falta casos por tipo de  operacion)
      2.4 - Mostrar el subtotal del concepto al usuario.
    
    3 - Función ActializarFecha
      3.1 - Obtener el valor de la fecha de la nomina
      3.2 - Obtener año y mes de la fecha
      3.3 - Dar formato al mes de la fecha
      3.4 - Enviar valores del mes y año a la base de datos

--- app/javascript/controllers/varconcepto_controller.js
+++ app/javascript/controllers/varconcepto_controller.js
@@ -8,17 +8,40 @@ export default class extends Controller {
   		    "nominaespecifica",
   		    "estatusConcepto",
   		    "indicadorPago",
-  		    "inidiceNomina" ]
+  		    "inidiceNomina",
+	  	    "montoPrevio",
+  		    "subTotal",
+  		    "saldo",
+  		    "mes",
+  		    "ano",
+  		    "indiceConcepto" ]
   seleccionar(){
+    const opciones = {
+	    M: "MONTO",
+	    C: "CANTIDAD",
+	    P: "PORCENTAJE"
+    }
     const selecto = this.codigoselectoTarget.value
     const opcionselecta = this.codigoselectoTarget.options[this.codigoselectoTarget.selectedIndex]
     const description = opcionselecta.dataset.descripcion
     const estatus = opcionselecta.dataset.estatus
     const indicador = opcionselecta.dataset.indc
-    const indicadorPago = document.getElementById("variacion_indicador_de_pago") 
+    const montoFijo = opcionselecta.dataset.monto
+    const operacion = opcionselecta.dataset.operacion
+    const indice = opcionselecta.dataset.indice
+    const indicadorPago = this.indicadorPagoTarget 
+    const montoConceptoPrevio = this.montoPrevioTarget
+    const saldo = this.saldoTarget
+    const subTotal = this.subTotalTarget
     this.descripcionselectoTarget.value = description
     this.estatusConceptoTarget.value = estatus
-    indicadorPago.value = indicador
+    this.indicadorPagoTarget.value = indicador
+    this.indiceConceptoTarget.value = indice
+    indicadorPago.value = opciones[indicador]
+    montoConceptoPrevio.value = montoFijo
+    saldo.value =  "0.0"
+    subTotal.value = "0.0"
+    
  } 
 
   cargarNominaEspecifica(e){
@@ -35,6 +58,26 @@ export default class extends Controller {
 	    cargarNominaEspc.appendChild(option);
 	  });
 	});
+  }
+  
+  calculo(e){
+    const montoConceptoPrevio = this.montoPrevioTarget.value     
+    const montoSaldo = parseFloat(e.target.value)
+    const montoPrev = parseFloat(montoConceptoPrevio)
+    const subTotal = montoSaldo * montoPrev 
+    this.subTotalTarget.value = subTotal
+  }
+
+  actualizarFecha(e){
+    const fecha = e.target.value
+    if(fecha){
+      const date = new Date(fecha)
+      const month = date.getMonth() + 1
+      const year = date.getFullYear()  
+      const formattedMonth = month.toString().padStart(2, '0')
+      this.mesTarget.value = parseInt(formattedMonth)
+      this.anoTarget.value = parseInt(year)
+    }
   }
 
 }
