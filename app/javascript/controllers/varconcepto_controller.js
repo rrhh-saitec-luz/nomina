4759cb67268fcc21d780f00ff48b54e8c29bdae0
saitec <dlinares@rrhh.luz.edu.ve>
2025-05-08 10:59:38 -0400

    1.- Crear vista _cabecera, para mostrar cabecera de la variacion, contiene datos del trabajador no relacionados con la forma. 2.- Agregar funciones calculo y fecha, para calcular el valor del monto a pagar y el mes y año de la nomina que se trabaja. 3.- Arreglar vista variacions/new para facilitar lectura de codigo.up

--- app/javascript/controllers/varconcepto_controller.js
+++ app/javascript/controllers/varconcepto_controller.js
@@ -14,18 +14,18 @@ export default class extends Controller {
   		    "saldo",
   		    "mes",
   		    "ano",
-  		    "indiceConcepto" ]
+  		    "indiceConcepto",
+  		    "indicadorReal" ]
   seleccionar(){
     const opciones = {
 	    M: "MONTO",
 	    C: "CANTIDAD",
 	    P: "PORCENTAJE"
     }
-    const selecto = this.codigoselectoTarget.value
     const opcionselecta = this.codigoselectoTarget.options[this.codigoselectoTarget.selectedIndex]
     const description = opcionselecta.dataset.descripcion
     const estatus = opcionselecta.dataset.estatus
-    const indicador = opcionselecta.dataset.indc
+    const indicador = opcionselecta.dataset.indicador
     const montoFijo = opcionselecta.dataset.monto
     const operacion = opcionselecta.dataset.operacion
     const indice = opcionselecta.dataset.indice
@@ -37,11 +37,11 @@ export default class extends Controller {
     this.estatusConceptoTarget.value = estatus
     this.indicadorPagoTarget.value = indicador
     this.indiceConceptoTarget.value = indice
+    this.indicadorRealTarget.value = indicador
     indicadorPago.value = opciones[indicador]
     montoConceptoPrevio.value = montoFijo
     saldo.value =  "0.0"
-    subTotal.value = "0.0"
-    
+    subTotal.value = montoFijo 
  } 
 
   cargarNominaEspecifica(e){
@@ -54,24 +54,38 @@ export default class extends Controller {
 	  datos.forEach(nomina =>{
 	    const option = document.createElement('option');
 	    option.value = nomina.tipo_nomina_especifica;
-	    option.textContent = nomina.descripcion;
+	    option.textContent = nomina.descripcion
 	    cargarNominaEspc.appendChild(option);
 	  });
-	});
+	})
+        .catch(error => {
+          console.error('Error al cargar los datos:', error);
+        });
   }
   
-  calculo(e){
+  calculoMontoSaldo(e){
+    const obtenerIndicador = this.indicadorPagoTarget.value
     const montoConceptoPrevio = this.montoPrevioTarget.value     
     const montoSaldo = parseFloat(e.target.value)
     const montoPrev = parseFloat(montoConceptoPrevio)
-    const subTotal = montoSaldo * montoPrev 
-    this.subTotalTarget.value = subTotal
+    if(obtenerIndicador == "PORCENTAJE"){
+      const subTotal = (montoPrev * montoSaldo)/100 
+      this.subTotalTarget.value = subTotal
+    }else{
+      const  subTotal = (montoPrev * montoSaldo) 
+      this.subTotalTarget.value = subTotal
+    }
+  }
+  calculoMontoConcepto(){
+    const montoSub = this.subTotalTarget
+    const montoConcepto = this.montoPrevioTarget.value
+    montoSub.value = montoConcepto
   }
 
   actualizarFecha(e){
     const fecha = e.target.value
     if(fecha){
-      const date = new Date(fecha)
+     const date = new Date(fecha)
       const month = date.getMonth() + 1
       const year = date.getFullYear()  
       const formattedMonth = month.toString().padStart(2, '0')
