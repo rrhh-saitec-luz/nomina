aca1b15957e0c26d98c1e2d97e84d4b7d9a3fde5
saitec <dlinares@rrhh.luz.edu.ve>
2025-06-10 08:49:17 -0400

    agregar ruta

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,7 +10,10 @@ class AdminsController < ApplicationController
   end
 
   def prenomina
-    registros_filtrados = Concepto.where(ANO: params[:year], MES: params[:month])
+    registros_filtrados = Concepto.where(ANO: params[:year],
+                                         MES: params[:month],
+                                         TIPO_NOMINA: params[:tpn],
+                                         TIPO_NOMINA_ESPACIFICA: params[:tpns])
     registros_filtrados.find_in_batches(batch_size: 1000) do |batch|
       nuevos_registros = batch.map do |registro|
         HistoricoPago.new(registro.attributes)
@@ -19,5 +22,14 @@ class AdminsController < ApplicationController
     end
     flash[:notice] = 'Proceso finalizado correctamente.'
     redirect_to admins_path
+  end
+
+  def modificar_prenomina
+    @prenomina = HistoricoPago.all
+    @meses = Concepto.all.select(:MES).distinct.pluck(:MES)
+    @years = Concepto.all.select(:ANO).distinct.pluck(:ANO)
+    @nominas = NominaTipo.all.select(:tipo_nomina).distinct.pluck(:tipo_nomina)
+    @especificas = NominaEspecifica.all.select(:tipo_nomina_especifica).distinct.pluck(:tipo_nomina_especifica)
+    render partial: 'admins/parciales/modificar_prenomina'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
       get 'index', action: :index
       get 'generar_nomina', action: :generar_nomina
       post 'prenomina', action: :prenomina
+      get 'modificar_prenomina', action: :modificar_prenomina
     end
   end
 
