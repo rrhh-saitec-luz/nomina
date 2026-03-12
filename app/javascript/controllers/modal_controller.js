2ad1a005bdf08bf9e5e0b9e52aa66beef10a11e9
saitec <dlinares@rrhh.luz.edu.ve>
2026-03-12 11:34:31 -0400

    modificados:     app/javascript/controllers/modal_controller.js modificados:     app/views/admins/detalles.html.erb modificados:     app/views/admins/parciales/_modal_detalle_cargos_prenomina.html.erb modificados:     app/views/admins/parciales/_modal_detalle_cargos_sistema.html.erb modificados:     app/views/admins/parciales/_multiples.html.erb modificados:     app/views/admins/parciales/_multiples_cargo_detalles.html.erb modificados:     app/views/admins/parciales/_multiples_tabla_detalles.html.erb
    Se modìficaron los archivos para solucionar problema de navegación con modal
    errores que ocurren al aplicar bootstrap con turbo, solucionado el mal comportamineto

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,31 +1,43 @@
-// app/javascript/controllers/modal_controller.js
 import { Controller } from "@hotwired/stimulus"
-// Si usas importmaps, asegúrate de que bootstrap esté disponible globalmente
-// Si usas esbuild/webpack: import { Modal } from "bootstrap"
 
 export default class extends Controller {
   connect() {
-    // Creamos la instancia
-    this.modal = new bootstrap.Modal(this.element)
-    this.modal.show()
+    // Inicializamos solo si el elemento existe
+    if (this.element) {
+      this.modal = new bootstrap.Modal(this.element)
+      this.modal.show()
+    }
   }
 
-  // Se ejecuta automáticamente cuando Turbo quita el elemento del DOM
   disconnect() {
-    this.modal.hide()
-    
-    // Limpieza manual del backdrop si Bootstrap lo deja olvidado
-    const backdrop = document.querySelector('.modal-backdrop')
-    if (backdrop) backdrop.remove()
-    
-    // Limpieza de clases en el body para permitir scroll
+    try {
+      if (this.modal) {
+        // Primero ocultamos rápido
+        this.modal.hide()
+        // Luego destruimos la instancia para liberar memoria
+        this.modal.dispose()
+      }
+    } catch (error) {
+      // Silenciamos el error de Bootstrap porque el elemento ya no está en el DOM
+      console.log("Bootstrap intentó acceder a un elemento ya removido por Turbo.")
+    } finally {
+      // Pase lo que pase, limpiamos el body y el backdrop
+      this.cleanup()
+    }
+  }
+
+  cerrar() {
+    if (this.modal) {
+      this.modal.hide()
+    }
+  }
+
+  cleanup() {
+    // Eliminamos cualquier rastro visual
+    document.querySelectorAll('.modal-backdrop').forEach(el => el.remove())
     document.body.classList.remove('modal-open')
     document.body.style.removeProperty('padding-right')
     document.body.style.removeProperty('overflow')
   }
-
-  // Método para cerrar manualmente desde botones de "Cerrar"
-  cerrar() {
-    this.modal.hide()
-  }
 }
+
